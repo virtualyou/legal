ea5865b08fd4e9f16468a97e126160db6bb6bd55
David L Whitehurst <me@dlwhitehurst.com>
2024-03-14 21:50:05 -0400

    remove swagger for issue

--- src/app.ts
+++ src/app.ts
@@ -26,10 +26,6 @@ import docRouter from "./routes/doc.routes";
 import cookieSession from "cookie-session";
 import * as process from "process";
 
-// swagger api documentation
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
-
 const initIndex = process.argv.indexOf("--init=true");
 const init = initIndex !== -1;
 
@@ -63,9 +59,6 @@ app.use(function (_req, res, next) {
 app.get("/", (_req, res) => {
     res.send("Welcome to the VirtualYou Legal API.");
 });
-
-// swagger path to api documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // database
 const Doc = db.doc;
